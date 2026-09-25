4affd8657976ba9717a1fa059bb4d40165e6532e
Om Ashok Akolkar <omakolkar4@gmail.com>
2026-09-25 15:26:14 +0530

    Update __init__.py

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,1 +1,10 @@
-from EmotionDetection.emotion_detection import emotion_detector
+"""
+EmotionDetection package.
+
+This package provides the emotion_detector function
+from the emotion_detection application module.
+"""
+
+from .emotion_detection import emotion_detector
+
+__all__ = ["emotion_detector"]
