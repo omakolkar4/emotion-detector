d5dea81908db42dd9bdc194c64542b6c78c82e6f
Om Ashok Akolkar <omakolkar4@gmail.com>
2026-09-25 15:34:33 +0530

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,4 +1,5 @@
 import unittest
+
 from EmotionDetection.emotion_detection import emotion_detector
 
 
@@ -6,23 +7,23 @@ class TestEmotionDetector(unittest.TestCase):
 
     def test_anger(self):
         result = emotion_detector("I am furious about this")
-        self.assertIsNotNone(result)
+        self.assertEqual(result["dominant_emotion"], "anger")
 
     def test_disgust(self):
         result = emotion_detector("This is disgusting")
-        self.assertIsNotNone(result)
+        self.assertEqual(result["dominant_emotion"], "disgust")
 
     def test_fear(self):
         result = emotion_detector("I am afraid")
-        self.assertIsNotNone(result)
+        self.assertEqual(result["dominant_emotion"], "fear")
 
     def test_joy(self):
         result = emotion_detector("I am very happy")
-        self.assertIsNotNone(result)
+        self.assertEqual(result["dominant_emotion"], "joy")
 
     def test_sadness(self):
         result = emotion_detector("I am feeling sad")
-        self.assertIsNotNone(result)
+        self.assertEqual(result["dominant_emotion"], "sadness")
 
 
 if __name__ == "__main__":
