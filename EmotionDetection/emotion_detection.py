3300e7e9938d7000702a15563320df02892bfc15
Om Ashok Akolkar <omakolkar4@gmail.com>
2026-09-25 14:38:53 +0530

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,1 +1,19 @@
+import requests
 
+
+def emotion_detector(text_to_analyse):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    myobj = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    header = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    response = requests.post(url, json=myobj, headers=header, timeout=30)
+
+    return response.json()
